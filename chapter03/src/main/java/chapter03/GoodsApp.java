6fe2021a5ca77ce3d60f0c0534cc7d188753af86
ryujiung <ryujiung@localhost>
2023-08-09 12:51:06 +0900

    수

---
+++ chapter03/src/main/java/chapter03/GoodsApp.java
@@ -0,0 +1,25 @@
+package chapter03;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Goods camera = new Goods();
+		camera.setName("Nikon");
+		camera.setPrice(40000);
+		camera.setCountStock(10);
+		camera.setCountSold(20);
+		
+		// 정보은닉(데이터 보호) 
+		camera.setPrice(-1);
+		
+		//countOfGoods test
+		Goods goods1 = new Goods();
+		Goods goods2 = new Goods();
+		Goods goods3 = new Goods();
+		
+		System.out.println(Goods.countOfGoods);
+		System.out.println("상품이름: "+camera.getName()+", 가격: "+camera.getPrice()+", 재고개수: "+camera.getCountStock()+", 팔린개수: "+camera.getCountSold());
+	}
+
+}
