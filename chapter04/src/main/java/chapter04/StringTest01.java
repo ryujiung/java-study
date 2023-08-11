e63457aef7377bb90b133007190d206e9fc09203
ryujiung <ryujiung@localhost>
2023-08-11 12:48:15 +0900

    immutability

---
+++ chapter04/src/main/java/chapter04/StringTest01.java
@@ -0,0 +1,25 @@
+package chapter04;
+
+public class StringTest01 {
+
+	public static void main(String[] args) {
+		// c:\temp
+		System.out.println("c:\\temp");
+		
+		// "Hello"
+		System.out.println("\"hello");
+		
+		// control character
+	
+		// \t: tab
+		// \r: carriage
+		// \n: new line
+		// \b: bell
+		
+		char c = '\'';
+		System.out.println(c);
+		
+		String path =("/poscodx2023/download");
+	}
+
+}
