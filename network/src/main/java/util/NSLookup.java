54486a3d14584d8cae325ad8e96b92be37aaba3c
ryujiung <ryujiung@localhost>
2023-08-17 15:48:49 +0900

    Inetaddress

---
+++ network/src/main/java/util/NSLookup.java
@@ -0,0 +1,23 @@
+package util;
+
+import java.util.*;
+
+public class NSLookup {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		while(true) {
+			System.out.print(">>");
+			String line = scanner.nextLine();
+			
+			if("quit".equals(line)) {
+				break;
+			}
+			
+			
+		}
+
+	}
+
+}
