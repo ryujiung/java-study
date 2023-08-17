cd45e385ec5af8bfde4841f08d6521efc78db833
ryujiung <ryujiung@localhost>
2023-08-17 16:03:40 +0900

    NSLookup practice

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -1,5 +1,7 @@
 package util;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.*;
 
 public class NSLookup {
@@ -10,6 +12,15 @@ public class NSLookup {
 		while(true) {
 			System.out.print(">>");
 			String line = scanner.nextLine();
+			try {
+				InetAddress[] Ipaddresses = InetAddress.getAllByName(line);
+				
+				for(InetAddress Ipaddress : Ipaddresses) {
+					System.out.println(line+": "+Ipaddress.getHostAddress());
+				}
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			}
 			
 			if("quit".equals(line)) {
 				break;
