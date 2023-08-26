0ed270adf5c6f4891df64477bd789307222f9c82
ryujiung <ryujiung@localhost>
2023-08-26 20:58:53 +0900

    chat gui server

---
+++ network/src/main/java/chat/gui/ChatServer.java
@@ -0,0 +1,54 @@
+package chat.gui;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatServer {
+	private static final int PORT = 8887;
+	public static void main(String[] args) {
+		List<PrintWriter> listPrintWriter = new ArrayList<PrintWriter>();
+		ServerSocket serversocket = null;
+		
+		try {
+			serversocket = new ServerSocket();
+			
+			serversocket.setReuseAddress(true);
+			
+			String localhost = InetAddress.getLocalHost().getHostAddress();
+			serversocket.bind(new InetSocketAddress("0.0.0.0", PORT),10);
+			System.out.println("chatserver starts at"+PORT);
+			
+			while(true) {
+				Socket socket = serversocket.accept();
+				
+				Thread thread = new ChatServerThread(socket,listPrintWriter);
+				thread.start();
+			}
+						
+		}catch(SocketException e){
+			e.printStackTrace();
+		}
+		catch(IOException e) {
+			System.out.println("error"+e);
+		}finally {
+			if(serversocket != null && serversocket.isClosed() == false) {
+				try {
+					serversocket.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+
+	}
+
+}
